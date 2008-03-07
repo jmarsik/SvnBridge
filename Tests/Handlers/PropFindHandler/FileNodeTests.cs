799fec238744514009d3d927234420895f18d4b4
Ayende_cp <SND\Ayende_cp>
2008-03-06 19:57:34 -0800

    fixing unit test

--- Tests/Handlers/PropFindHandler/FileNodeTests.cs
+++ Tests/Handlers/PropFindHandler/FileNodeTests.cs
@@ -17,7 +17,7 @@ namespace SvnBridge.Handlers
             item.Name = "A !@#$%^&()_-+={[}];',~`..txt";
             FileNode node = new FileNode(item, null);
 
-            string result = node.GetProperty(null, xml.CreateElement("baseline-relative-path"));
+            string result = node.GetProperty(new GetHandler(), xml.CreateElement("baseline-relative-path"));
 
             Assert.AreEqual(
                 "<lp2:baseline-relative-path>A !@#$%^&amp;()_-+={[}];',~`..txt</lp2:baseline-relative-path>", result);
@@ -32,7 +32,7 @@ namespace SvnBridge.Handlers
             item.Name = "A !@#$%^&()_-+={[}];',~`..txt";
             FileNode node = new FileNode(item, null);
 
-            string result = node.GetProperty(null, xml.CreateElement("checked-in"));
+            string result = node.GetProperty(new GetHandler(), xml.CreateElement("checked-in"));
 
             Assert.AreEqual(
                 "<lp1:checked-in><D:href>/!svn/ver/5718/A%20!@%23$%25%5E&amp;()_-+=%7B%5B%7D%5D%3B',~%60..txt</D:href></lp1:checked-in>",
