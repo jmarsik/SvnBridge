f88261e643d0e89db1a420edd2141aaa9f6a02ea
jwanagel_cp <SND\jwanagel_cp>
2008-03-17 02:27:23 -0700

    Updated build script to fix build server

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off  
-%windir%\Microsoft.NET\Framework\v2.0.50727\MSBuild.exe SvnBridge.msbuild /p:Configuration=Debug /p:CodePlex3rdParty=3rdParty /t:%*
+%windir%\Microsoft.NET\Framework\v2.0.50727\MSBuild.exe SvnBridge.msbuild /p:Configuration=Debug /p:CodePlex3rdParty=..\3rdParty /t:%*
