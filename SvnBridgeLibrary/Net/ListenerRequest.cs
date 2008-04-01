b77c0482a0dce37bac461b6234a6d4eb502f6928
Ayende_cp <SND\Ayende_cp>
2008-04-01 15:25:56 -0700

    More robust handling of headers (for Subversive)

--- SvnBridgeLibrary/Net/ListenerRequest.cs
+++ SvnBridgeLibrary/Net/ListenerRequest.cs
@@ -1,201 +1,207 @@
 using System;
 using System.Collections.Specialized;
 using System.IO;
+using System.Net;
 using System.Text;
 
 namespace SvnBridge.Net
 {
-    public class ListenerRequest : IHttpRequest
-    {
-        private readonly NameValueCollection headers;
-        private string httpMethod;
-        private MemoryStream inputStream;
-        private string path;
-        private Uri url;
+	public class ListenerRequest : IHttpRequest
+	{
+		private readonly NameValueCollection headers;
+		private string httpMethod;
+		private MemoryStream inputStream;
+		private string path;
+		private Uri url;
 
-        public ListenerRequest(Stream stream)
-        {
-            headers = new NameValueCollection();
+		public ListenerRequest(Stream stream)
+		{
+			headers = new NameValueCollection();
 
-            ParseRequest(stream);
-        }
+			ParseRequest(stream);
+		}
 
 
-        public string ApplicationPath
-        {
-            get { return "/"; }
-        }
+		public string ApplicationPath
+		{
+			get { return "/"; }
+		}
 
-        public NameValueCollection Headers
-        {
-            get { return headers; }
-        }
+		public NameValueCollection Headers
+		{
+			get { return headers; }
+		}
 
-        public string HttpMethod
-        {
-            get { return httpMethod; }
-        }
+		public string HttpMethod
+		{
+			get { return httpMethod; }
+		}
 
-        public Stream InputStream
-        {
-            get { return inputStream; }
-        }
+		public Stream InputStream
+		{
+			get { return inputStream; }
+		}
 
-        public Uri Url
-        {
-            get
-            {
-                if (url == null)
-                {
-                    BuildUrl();
-                }
+		public Uri Url
+		{
+			get
+			{
+				if (url == null)
+				{
+					BuildUrl();
+				}
 
-                return url;
-            }
-        }
+				return url;
+			}
+		}
 
-        public string LocalPath
-        {
-            get
-            {
-                return Url.LocalPath;
-            }
-        }
+		public string LocalPath
+		{
+			get
+			{
+				return Url.LocalPath;
+			}
+		}
 
-        private void BuildUrl()
-        {
-            string host = Headers["host"];
+		private void BuildUrl()
+		{
+			string host = Headers["host"];
 
-            if (!String.IsNullOrEmpty(host) && !path.StartsWith("http"))
-            {
-                url = new Uri(String.Format("http://{0}{1}", host, path));
-            }
-            else
-            {
-                url = new Uri(path);
-            }
-        }
+			if (!String.IsNullOrEmpty(host) && !path.StartsWith("http"))
+			{
+				url = new Uri(String.Format("http://{0}{1}", host, path));
+			}
+			else
+			{
+				url = new Uri(path);
+			}
+		}
 
-        private void ParseRequest(Stream stream)
-        {
-            MemoryStream buffer = new MemoryStream();
+		private void ParseRequest(Stream stream)
+		{
+			MemoryStream buffer = new MemoryStream();
 
-            ReadToBuffer(stream, buffer);
+			ReadToBuffer(stream, buffer);
 
-            string startLine = ReadLine(stream, buffer);
-            ParseStartLine(startLine);
+			string startLine = ReadLine(stream, buffer);
+			ParseStartLine(startLine);
 
-            string headerLine = ReadLine(stream, buffer);
-            while (headerLine != String.Empty)
-            {
-                ParseHeaderLine(headerLine);
-                headerLine = ReadLine(stream, buffer);
-            }
+			string headerLine = ReadLine(stream, buffer);
+			while (headerLine != String.Empty)
+			{
+				ParseHeaderLine(headerLine);
+				headerLine = ReadLine(stream, buffer);
+			}
 
-            ReadMessageBody(stream, buffer);
-        }
+			ReadMessageBody(stream, buffer);
+		}
 
-        private static string ReadLine(Stream stream,
-                                       MemoryStream buffer)
-        {
-            int offset = (int) buffer.Position;
+		private static string ReadLine(Stream stream,
+									   MemoryStream buffer)
+		{
+			int offset = (int)buffer.Position;
 
-            int previousByte = -1;
-            int nextByte = buffer.ReadByte();
+			int previousByte = -1;
+			int nextByte = buffer.ReadByte();
 
-            while (!(previousByte == 13 && nextByte == 10))
-            {
-                int byteRead = buffer.ReadByte();
+			while (!(previousByte == 13 && nextByte == 10))
+			{
+				int byteRead = buffer.ReadByte();
 
-                if (byteRead == -1)
-                {
-                    ReadToBuffer(stream, buffer);
-                }
-                else
-                {
-                    previousByte = nextByte;
-                    nextByte = byteRead;
-                }
-            }
+				if (byteRead == -1)
+				{
+					ReadToBuffer(stream, buffer);
+				}
+				else
+				{
+					previousByte = nextByte;
+					nextByte = byteRead;
+				}
+			}
 
-            return Encoding.ASCII.GetString(buffer.GetBuffer(), offset, (int) buffer.Position - offset - 2);
-        }
+			return Encoding.ASCII.GetString(buffer.GetBuffer(), offset, (int)buffer.Position - offset - 2);
+		}
 
-        private static void ReadToBuffer(Stream stream,
-                                         MemoryStream buffer)
-        {
-            int originalPosition = (int) buffer.Position;
+		private static void ReadToBuffer(Stream stream,
+										 MemoryStream buffer)
+		{
+			int originalPosition = (int)buffer.Position;
 
-            byte[] bytes = new byte[Constants.BufferSize];
+			byte[] bytes = new byte[Constants.BufferSize];
 
-            int bytesRead = stream.Read(bytes, 0, bytes.Length);
+			int bytesRead = stream.Read(bytes, 0, bytes.Length);
 
-            int availableCapacity = buffer.Capacity - (int) buffer.Length;
+			int availableCapacity = buffer.Capacity - (int)buffer.Length;
 
-            if (availableCapacity < bytesRead)
-            {
-                buffer.Capacity += (bytesRead - availableCapacity);
-            }
+			if (availableCapacity < bytesRead)
+			{
+				buffer.Capacity += (bytesRead - availableCapacity);
+			}
 
-            buffer.Position = buffer.Length;
+			buffer.Position = buffer.Length;
 
-            buffer.Write(bytes, 0, bytesRead);
+			buffer.Write(bytes, 0, bytesRead);
 
-            buffer.Position = originalPosition;
-        }
+			buffer.Position = originalPosition;
+		}
 
-        private void ReadMessageBody(Stream stream,
-                                     MemoryStream buffer)
-        {
-            int contentLength = GetContentLength();
+		private void ReadMessageBody(Stream stream,
+									 MemoryStream buffer)
+		{
+			int contentLength = GetContentLength();
 
-            bool finished = ((buffer.Length - buffer.Position) >= contentLength);
+			bool finished = ((buffer.Length - buffer.Position) >= contentLength);
 
-            while (!finished)
-            {
-                ReadToBuffer(stream, buffer);
+			while (!finished)
+			{
+				ReadToBuffer(stream, buffer);
 
-                finished = ((buffer.Length - buffer.Position) >= contentLength);
-            }
+				finished = ((buffer.Length - buffer.Position) >= contentLength);
+			}
 
-            byte[] messageBody = new byte[contentLength];
+			byte[] messageBody = new byte[contentLength];
 
-            buffer.Read(messageBody, 0, messageBody.Length);
+			buffer.Read(messageBody, 0, messageBody.Length);
 
-            inputStream = new MemoryStream(messageBody, false);
-        }
+			inputStream = new MemoryStream(messageBody, false);
+		}
 
-        private int GetContentLength()
-        {
-            int contentLength = 0;
+		private int GetContentLength()
+		{
+			int contentLength = 0;
 
-            string contentLengthHeader = Headers["Content-Length"];
-            if (!String.IsNullOrEmpty(contentLengthHeader))
-            {
-                int.TryParse(contentLengthHeader, out contentLength);
-            }
+			string contentLengthHeader = Headers["Content-Length"];
+			if (!String.IsNullOrEmpty(contentLengthHeader))
+			{
+				int.TryParse(contentLengthHeader, out contentLength);
+			}
 
-            return contentLength;
-        }
+			return contentLength;
+		}
 
-        private void ParseStartLine(string startLine)
-        {
-            string[] startLineParts = startLine.Split(' ');
-            httpMethod = startLineParts[0].ToLowerInvariant();
-            path = startLineParts[1];
-            if (path.StartsWith("//"))
-            {
-                path = path.Substring(1);
-            }
-        }
+		private void ParseStartLine(string startLine)
+		{
+			string[] startLineParts = startLine.Split(' ');
+			httpMethod = startLineParts[0].ToLowerInvariant();
+			path = startLineParts[1];
+			if (path.StartsWith("//"))
+			{
+				path = path.Substring(1);
+			}
+		}
 
-        private void ParseHeaderLine(string headerLine)
-        {
-            string headerName = headerLine.Substring(0, headerLine.IndexOf(": "));
+		private void ParseHeaderLine(string headerLine)
+		{
+			int indexOf = headerLine.IndexOf(":");
+			if (indexOf == -1)
+				throw new ProtocolViolationException("Could not parse header line: " + headerLine);
 
-            string headerValue = headerLine.Substring(headerName.Length + 2);
+			string headerName = headerLine.Substring(0, indexOf);
+			string headerValue = null;
+			if (headerLine.Length >= indexOf + 2)
+				headerValue = headerLine.Substring(headerName.Length + 2);
 
-            Headers.Add(headerName, headerValue);
-        }
-    }
+			Headers.Add(headerName, headerValue);
+		}
+	}
 }
