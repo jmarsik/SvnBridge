4619e60bfdd5d2b8ee377d23e3552cc7adce11cc
jwanagel_cp <SND\jwanagel_cp>
2008-04-06 16:20:28 -0700

    Updated build.bat to not require any parameters for a normal compile.

--- build.bat
+++ build.bat
@@ -1,2 +1,10 @@
-@echo off  
+@echo off
+if "%1" == "" goto :Build
+
 %windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe SvnBridge.msbuild /p:Configuration=Debug /p:CodePlex3rdParty=..\3rdParty /t:%*
+goto :End
+
+:Build
+%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe SvnBridge.msbuild /p:Configuration=Debug /p:CodePlex3rdParty=..\3rdParty
+
+:End
