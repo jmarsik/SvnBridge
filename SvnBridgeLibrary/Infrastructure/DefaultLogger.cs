145bd74a0fc55c02aac7dc0a215ec185e40d2411
Ayende_cp <SND\Ayende_cp>
2008-04-25 16:02:27 -0700

    Default logger will now try to write to C:\Documents and Settings\[USERNAME]\Local Settings\Application Data\SvnBridge if it cannot write to the current directory. Work Item: 10045

--- SvnBridgeLibrary/Infrastructure/DefaultLogger.cs
+++ SvnBridgeLibrary/Infrastructure/DefaultLogger.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -8,102 +9,124 @@ using System.Configuration;
 
 namespace SvnBridge.Infrastructure
 {
-	public class DefaultLogger : ILogger, ICanValidateMyEnvironment
-	{
-		#region ILogger Members
+    public class DefaultLogger : ILogger, ICanValidateMyEnvironment
+    {
+        #region ILogger Members
+        string logPath;
 
-		public void Error(string message, Exception exception)
-		{
-			WebException we = exception as WebException;
-			if (we != null && we.Response != null)
-			{
-				HttpWebResponse hwr = we.Response as HttpWebResponse;
-				if (hwr != null && hwr.StatusCode != HttpStatusCode.Unauthorized)
-				{
-					using (StreamReader sr = new StreamReader(we.Response.GetResponseStream()))
-					{
-						StringBuilder sb = new StringBuilder(message);
-						sb.AppendLine(" Error page is:");
-						sb.AppendLine(sr.ReadToEnd());
-						message = sb.ToString();
-					}
-				}
-			}
-			Log("Error", message, exception.ToString());
-		}
+        public void Error(string message, Exception exception)
+        {
+            WebException we = exception as WebException;
+            if (we != null && we.Response != null)
+            {
+                HttpWebResponse hwr = we.Response as HttpWebResponse;
+                if (hwr != null && hwr.StatusCode != HttpStatusCode.Unauthorized)
+                {
+                    using (StreamReader sr = new StreamReader(we.Response.GetResponseStream()))
+                    {
+                        StringBuilder sb = new StringBuilder(message);
+                        sb.AppendLine(" Error page is:");
+                        sb.AppendLine(sr.ReadToEnd());
+                        message = sb.ToString();
+                    }
+                }
+            }
+            Log("Error", message, exception.ToString());
+        }
 
-		public void Info(string message, Exception exception)
-		{
-			Log("Info", message, exception.ToString());
-		}
+        public void Info(string message, Exception exception)
+        {
+            Log("Info", message, exception.ToString());
+        }
 
-		public void Trace(string message, params object[] args)
-		{
-			if (Logging.TraceEnabled == false)
-				return;
-			Log("Trace", string.Format(message, args), null);
-		}
+        public void Trace(string message, params object[] args)
+        {
+            if (Logging.TraceEnabled == false)
+                return;
+            Log("Trace", string.Format(message, args), null);
+        }
 
-		public void TraceMessage(string message)
-		{
-			if (Logging.TraceEnabled == false)
-				return;
-			Log("TraceMessage", message, null);
-		}
+        public void TraceMessage(string message)
+        {
+            if (Logging.TraceEnabled == false)
+                return;
+            Log("TraceMessage", message, null);
+        }
 
-		#endregion
+        #endregion
 
-		private static void Log(string level, string message, string exception)
-		{
-			try
-			{
-				WriteLogMessageWithNoExceptionHandling(level, message, exception);
-			}
-			catch (Exception)
-			{
-				//We don't have anything to do here, can't 
-				// fix errors in error handling code
-			}
-		}
+        private void Log(string level, string message, string exception)
+        {
+            try
+            {
+                WriteLogMessageWithNoExceptionHandling(level, message, exception);
+            }
+            catch (Exception)
+            {
+                //We don't have anything to do here, can't 
+                // fix errors in error handling code
+            }
+        }
 
-		private static void WriteLogMessageWithNoExceptionHandling(string level, string message, string exception)
-		{
-			XmlWriterSettings settings = new XmlWriterSettings();
-			settings.Indent = true;
-			settings.OmitXmlDeclaration = true;
+        private void WriteLogMessageWithNoExceptionHandling(string level, string message, string exception)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            settings.OmitXmlDeclaration = true;
 
-            string logPath = ConfigurationManager.AppSettings["LogPath"];
-            if (logPath == null)
-                logPath = "";
+            string logPath = GetLogPath();
 
             string logFile = Path.Combine(logPath, level + ".log");
 
-			using(StreamWriter text = File.AppendText(logFile))
-			using(XmlWriter writer = XmlWriter.Create(text,settings))
-			{
-				writer.WriteStartElement("log");
-				writer.WriteAttributeString("level", level);
-				WriteCDataElement(writer, "message", message);
-				if(string.IsNullOrEmpty(exception)==false)
-					WriteCDataElement(writer, "exception", exception);
-				writer.WriteEndElement();
-			}
-		}
+            using (StreamWriter text = File.AppendText(logFile))
+            using (XmlWriter writer = XmlWriter.Create(text, settings))
+            {
+                writer.WriteStartElement("log");
+                writer.WriteAttributeString("level", level);
+                WriteCDataElement(writer, "message", message);
+                if (string.IsNullOrEmpty(exception) == false)
+                    WriteCDataElement(writer, "exception", exception);
+                writer.WriteEndElement();
+            }
+        }
 
-		private static void WriteCDataElement(XmlWriter writer, string name, string message)
-		{
-			writer.WriteStartElement(name);
-			writer.WriteCData(message);
-			writer.WriteEndElement();
-		}
+        private string GetLogPath()
+        {
+            if (logPath != null)
+                return logPath;
+            logPath = ConfigurationManager.AppSettings["LogPath"];
+            if (logPath != null)
+                return logPath;
+            logPath = "";
+            try
+            {
+                File.WriteAllText("tmp.log", "test");
+                File.Delete("tmp.log");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                logPath = Path.Combine(localAppData, "SvnBridge");
+                if (Directory.Exists(logPath) == false)
+                    Directory.CreateDirectory(logPath);
+            }
+            return logPath;
+        }
 
-		#region ICanValidateMyEnvironment Members
+        private static void WriteCDataElement(XmlWriter writer, string name, string message)
+        {
+            writer.WriteStartElement(name);
+            writer.WriteCData(message);
+            writer.WriteEndElement();
+        }
 
-		public void ValidateEnvironment()
-		{
-			WriteLogMessageWithNoExceptionHandling("test", "can write to file", null);
-		}
+        #region ICanValidateMyEnvironment Members
 
-		#endregion
-	}
+        public void ValidateEnvironment()
+        {
+            WriteLogMessageWithNoExceptionHandling("test", "can write to file", null);
+        }
+
+        #endregion
+    }
 }
