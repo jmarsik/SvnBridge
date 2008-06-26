041c7d9bcc682f343c7502d46d521d9283e87205
Ayende_cp <SND\Ayende_cp>
2008-06-26 00:35:52 -0700

    Editing using git

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 @echo off
 if NOT "%1" == "" goto :Build
 
+REM Testing that we can commit from git
+
 %windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe SvnBridge.msbuild /p:Configuration=Debug /p:CodePlex3rdParty=..\3rdParty
 goto :End
 
