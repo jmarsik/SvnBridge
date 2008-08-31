cf02b6f034993684bbe96a6fb162dca7ad9c38a4
jwanagel_cp <SND\jwanagel_cp>
2008-08-31 16:44:48 -0700

    Refactoring

--- SvnBridgeLibrary/BootStrapper.cs
+++ SvnBridgeLibrary/BootStrapper.cs
@@ -11,30 +11,12 @@ using SvnBridge.Infrastructure.Statistics;
 using SvnBridge.Interfaces;
 using SvnBridge.Net;
 using SvnBridge.SourceControl;
+using SvnBridge.Cache;
 
 namespace SvnBridge
 {
     public class BootStrapper
     {
-        private readonly Assembly[] assemblies;
-        private readonly string[] componentInterfacesNamespace;
-
-        private readonly Type[] representiveComponents = new Type[]
-            {
-                // representive interfaces
-                typeof (ICache),
-                typeof (ISourceControlProvider),
-                typeof (IRepositoryWebSvcFactory),
-                typeof (IRegistrationWebSvcFactory),
-                typeof (IRegistrationService),
-                typeof (IFileSystem),
-                typeof (IWebTransferService),
-                typeof (ITFSSourceControlService),
-                typeof (IAssociateWorkItemWithChangeSet),
-                typeof (IListener),
-                typeof (IActionTracking)
-            };
-
         public BootStrapper()  
 		{
             IoC.Container.Configuration["fileCachePath"] = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "FileCache");
@@ -42,68 +24,31 @@ namespace SvnBridge
                 Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "MetaDataCache");
 
         	TfsUtil.OnSetupWebRequest = WebRequestSetup.OnWebRequest;
-
-            List<Assembly> asms = new List<Assembly>();
-            List<string> names = new List<string>();
-            foreach (Type type in representiveComponents)
-            {
-                if (asms.Contains(type.Assembly) == false)
-                {
-                    asms.Add(type.Assembly);
-                }
-
-                if (type.IsInterface) // we perform auto registration for interface only
-                {
-                    names.Add(type.Namespace);
-                }
-            }
-
-            componentInterfacesNamespace = names.ToArray();
-            assemblies = asms.ToArray();
         }
 
         public void Start()
         {
-            RegisterTypesFromKnownAssemblies();
-        	RegisterTypeFromAddinAssemblies();
             RegisterType(typeof(FileRepository), typeof(FileRepository));
+            RegisterType(typeof(ISourceControlServicesHub), typeof(SourceControlServicesHub));
+            RegisterType(typeof(IFileCache), typeof(FileCache));
+            RegisterType(typeof(IPersistentCache), typeof(MemoryBasedPersistentCache));
+            RegisterType(typeof(ILogger), typeof(DefaultLogger));
+            RegisterType(typeof(IIgnoredFilesSpecification), typeof(OldSvnBridgeFilesSpecification));
+            RegisterType(typeof(IActionTracking), typeof(ActionTrackingViaPerfCounter));
+            RegisterType(typeof(IMetaDataRepositoryFactory), typeof(MetaDataRepositoryFactory));
+            RegisterType(typeof(IListener), typeof(Listener));
+            RegisterType(typeof(IAssociateWorkItemWithChangeSet), typeof(AssociateWorkItemWithChangeSet));
+            RegisterType(typeof(ICache), typeof(WebCache));
+            RegisterType(typeof(ISourceControlProvider), typeof(TFSSourceControlProvider));
+            RegisterType(typeof(ITFSSourceControlService), typeof(TFSSourceControlService));
+            RegisterType(typeof(IProjectInformationRepository), typeof(ProjectInformationRepository));
+            RegisterType(typeof(ITfsUrlValidator), typeof(TfsUrlValidator));
+            RegisterType(typeof(IRegistrationService), typeof(RegistrationService));
+            RegisterType(typeof(IWebTransferService), typeof(WebTransferService));
+            RegisterType(typeof(IRegistrationWebSvcFactory), typeof(RegistrationWebSvcFactory));
+            RegisterType(typeof(IRepositoryWebSvcFactory), typeof(RepositoryWebSvcFactory));
+            RegisterType(typeof(IFileSystem), typeof(FileSystem));
         }
-
-    	private void RegisterTypeFromAddinAssemblies()
-    	{
-    		string[] assemblyNames = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll");
-    		foreach (string assemblyName in assemblyNames)
-    		{
-				// Here we assume that the assembly name and the file name are identical.
-    			Assembly assembly = Assembly.Load(Path.GetFileNameWithoutExtension(assemblyName));
-				// we ignore the well known assemblies
-				if(Array.IndexOf(assemblies, assembly)!=-1)
-					continue;
-
-				foreach (Type type in assembly.GetTypes())
-    			{
-					if (typeof(IAssemblyAddin).IsAssignableFrom(type))
-    				{
-    					IAssemblyAddin addin = (IAssemblyAddin) Activator.CreateInstance(type);
-    					addin.Initialize(IoC.Container);
-    				}
-    			}
-    		}
-    	}
-
-    	private void RegisterTypesFromKnownAssemblies()
-    	{
-    		foreach(Type type in GetAllTypesFromAssemblies())
-    		{
-    			if (ValidTypeForRegistration(type) == false)
-    				continue;
-
-    			foreach (Type interfaceType in type.GetInterfaces())
-    			{
-                    RegisterType(interfaceType, type);
-    			}
-    		}
-    	}
 
         private void RegisterType(Type interfaceType, Type type)
         {
@@ -124,48 +69,6 @@ namespace SvnBridge
                     IoC.Container.Register(interfaceType, type, interceptors.ToArray());
                 }
             }
-        }
-
-    	private IEnumerable<Type> GetAllTypesFromAssemblies()
-    	{
-    		foreach (Assembly assembly in assemblies)
-    		{
-    			foreach (Type type in assembly.GetTypes())
-    			{
-    				yield return type;
-    			}
-    		}
-    	}
-
-    	private bool IsValidInterface(Type interfaceType)
-        {
-            return Array.IndexOf(componentInterfacesNamespace, interfaceType.Namespace) != -1 &&
-                   Array.IndexOf(assemblies, interfaceType.Assembly) != -1;
-        }
-
-
-        private bool ValidTypeForRegistration(Type type)
-        {
-            if (type.Namespace == "SvnBridge.NullImpl")
-                return false;
-
-            if (type.IsInterface || type.IsAbstract)
-            {
-                return false;
-            }
-            if (Array.IndexOf(representiveComponents, type) != -1)
-            {
-                return true;
-            }
-            Type[] interfaces = type.GetInterfaces();
-            foreach (Type interfaceType in interfaces)
-            {
-                if (IsValidInterface(interfaceType))
-                {
-                    return true;
-                }
-            }
-            return false;
         }
     }
 }
--- SvnBridgeLibrary/Interfaces/IAssemblyAddin.cs
+++
@@ -1,9 +0,0 @@
-using SvnBridge.Infrastructure;
-
-namespace SvnBridge.Interfaces
-{
-	public interface IAssemblyAddin
-	{
-		void Initialize(Container container);
-	}
-}
